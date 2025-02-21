14cf74d4806781c764423def871df103265ee3d0
min4937 <pmy1504@naver.com>
2025-02-21 23:11:38 +0900

    체력바 UI위치 개선
    카메라가 플레이어를 따라오는 과정에서 체력바가 화면에 고정되서 밀림. UI를 플레이어 위치에 고정시켜서 따라오는 스크립트 작성

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public RectTransform hpBar;
+    public Camera mainCamera;
+    public Vector3 offset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 worldPosition = target.position + offset;  // 대상 위치 + 오프셋
+        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(worldPosition); // 씬 카메라
+
+        Vector2 screenPosition = new Vector2(
+            ((viewportPosition.x * mainCamera.pixelWidth) - (mainCamera.pixelWidth * 0.5f)),
+            ((viewportPosition.y * mainCamera.pixelHeight) - (mainCamera.pixelHeight * 0.5f)));
+
+        hpBar.anchoredPosition = screenPosition;
+    }
+}
