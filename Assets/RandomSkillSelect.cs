0d1374c9683e83510c74292c2026875e94f6499c
ssolem <tjrals961@gmail.com>
2025-02-21 17:52:28 +0900

    [feat] Skill Script

---
+++ Assets/RandomSkillSelect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomSkillSelect : MonoBehaviour
+{
+    public List<BaseSkills> baseSkillsList { get; set; }
+    public Dictionary<int, BaseSkills> baseSkillDict { get; set; }
+
+
+    public void RandomSkillDraw()
+    {
+        
+    }
+}
---
+++ Assets/SkillData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SkillType
+{
+    AttackBoost,
+    AttackSpeedBoost,
+    CriticalBoost,
+    HealthBoost,
+    ProjectileUp,
+}
+public class SkillData : MonoBehaviour
+{
+    public int skillId;
+    public string skillName;
+    public string skillDescription;
+    public SkillType skillType;
+    public float skillValue;
+
+    public SkillData(int id, string name, string description, SkillType type, float value)
+    {
+        skillId = id;
+        skillName = name;
+        skillDescription = description;
+        skillType = type;
+        skillValue = value;
+    }
+
+
+}
