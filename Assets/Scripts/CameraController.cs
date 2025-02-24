6ab204b6a334f703e8a171da3e47b828823bc5a4
min4937 <pmy1504@naver.com>
2025-02-24 12:32:15 +0900

    경험치 상승 바 작성완료

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public float minX = -4f;  // 카메라가 이동할 수 있는 X축 최소값
-    public float maxX = 3f;   // 카메라가 이동할 수 있는 X축 최대값
-    public float minY = -2f;  // 카메라가 이동할 수 있는 Y축 최소값
-    public float maxY = 13f;   // 카메라가 이동할 수 있는 Y축 최대값
-    public float smoothSpeed = 0.2f; // 카메라 이동 속도
+    public float minX = -4f;  
+    public float maxX = 3f;   
+    public float minY = -2f;  
+    public float maxY = 13f;   
+    public float smoothSpeed = 0.2f; 
 
     void FixedUpdate()
     {
@@ -20,7 +20,7 @@ public class CameraController : MonoBehaviour
         pos.x = target.position.x;
         pos.y = target.position.y;
 
-        // 카메라 위치를 제한 영역 내로 조정
+        
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
--- Assets/Scripts/Hp_Bar.cs
+++ Assets/Scripts/Hp_Bar.cs
@@ -11,29 +11,27 @@ public class Hp_Bar : MonoBehaviour
     public float maxHp;
     public float currenthp;
     public TextMeshProUGUI nowHpText;
-    public float damageRate = 10f; // 초당 데미지
+    public float damageRate = 10f; 
 
     void Start()
     {
-        currenthp = maxHp;  // 현재 체력을 최대 체력으로 초기화
+        currenthp = maxHp;  
 
-        if (hpbar != null)  // null 체크 추가
+        if (hpbar != null)  
         {
-            hpbar.value = currenthp; // 슬라이더 초기 값 설정
+            hpbar.value = currenthp; 
         }
 
        
-        UpdateHpText(); // 초기 텍스트 업데이트
+        UpdateHpText(); 
     }
-
-
-    // Update is called once per frame
+        
     void Update()
     {
-        //transform.position = player.position + new Vector3(0, 0, 0);
+        
         if (hpbar != null)
         {
-            hpbar.value = currenthp; // currenthp를 직접 설정
+            hpbar.value = currenthp; 
         }
         TakeDamage(damageRate * Time.deltaTime);
 
@@ -54,7 +52,7 @@ public class Hp_Bar : MonoBehaviour
        
         if (nowHpText != null)
         {
-            nowHpText.text = currenthp.ToString("N0"); // 현재 체력을 문자열로 변환하여 표시
+            nowHpText.text = currenthp.ToString("N0"); 
         }
     }
 }
