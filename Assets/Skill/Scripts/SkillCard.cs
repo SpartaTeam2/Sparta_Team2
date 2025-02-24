8ca43d3f59817d68e0b368898f5ecf2b23e0db79
min4937 <pmy1504@naver.com>
2025-02-24 22:40:24 +0900

    스킬 아이콘 변경

--- Assets/Skill/Scripts/SkillCard.cs
+++ Assets/Skill/Scripts/SkillCard.cs
@@ -8,8 +8,8 @@ public class SkillCard : MonoBehaviour
 {
     public SkillData selectedSkillData;
 
-    public TextMeshProUGUI skillName;
-    public TextMeshProUGUI skillDescription;
+    public Text skillName;
+    public Text skillDescription;
 
     private Button selectButton;
     [SerializeField] private Image skillCardBackGround;
@@ -25,7 +25,7 @@ public class SkillCard : MonoBehaviour
         int locatecard = SkillHandler.Instance.selectedSkillNum;
         int cardtotal = SkillHandler.Instance.randomSkillNum;
 
-        skillCardBackGround.transform.position += new Vector3(-110 * (cardtotal - 1) + (220 * locatecard), 0, 0);
+        skillCardBackGround.transform.position += new Vector3(-220 * (cardtotal - 1) + (220 * locatecard), 0, 0);
     }
 
     public void OnClickSelect()
