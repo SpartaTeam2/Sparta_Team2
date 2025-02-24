44df426592074370411c2570e78eac8a52dc22d0
EEL_B <sang980203@gmail.com>
2025-02-24 15:17:51 +0900

    feat:  전투 사이클
    사운드, 이펙트, 사망 효과음 추가

--- Assets/Player/BulletCtrl.cs
+++ Assets/Player/BulletCtrl.cs
@@ -12,12 +12,20 @@ public class BulletCtrl : MonoBehaviour
     public float lifetime = 1.0f; // 투사체 유지 시간
     public float Damage;
     public float BulletSpeed;
+    //public AudioSource _audioSource;
+    //public AudioClip _bulletSound;
 
     public GameObject Attacker;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (BulletImage != null)
+        {
+            GetComponentInChildren<SpriteRenderer>().sprite = BulletImage; //있으면 변경
+        }
+        //if (_audioSource == null)
+        //    _audioSource = GetComponent<AudioSource>();
         Damage = Attacker.GetComponent<PlayerCtrl>().AttackDamage;
         Destroy(gameObject, lifetime); // 투사체 삭제
     }
@@ -30,9 +38,9 @@ public class BulletCtrl : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(Attacker.tag)) // If same gameobject
+        if (collision.CompareTag(Attacker.tag)) // 태그가 같으면
         {
-            // do nothing
+            // 아무것도 안하기
         }
         else
         {
--- Assets/Player/PlayerCtrl.cs
+++ Assets/Player/PlayerCtrl.cs
@@ -9,7 +9,7 @@ public class PlayerCtrl : MonoBehaviour
     public float MaxHp;
     public float HP;
     public float speed = 10.0f; //스피드
-    public float AttackDamage; 
+    public float AttackDamage;
 
     [Header("플레이어 공격 정보")]
     public GameObject BulletPrefab; // 투사체
@@ -17,11 +17,19 @@ public class PlayerCtrl : MonoBehaviour
     public float GunRate = 10f; //  발사 시간
     private float GunTimer; // 발사 타이머
 
+    [Header("플레이어 피격 정보")]
+    public GameObject HitEffect;
+
+    public AudioSource _audioSource;
+    public AudioClip DieSoundClip;
+
     Animator animator; // 애니메이터
 
     // Start is called before the first frame update
     void Start()
     {
+        if (_audioSource == null)
+            _audioSource = GetComponent<AudioSource>();
         animator = GetComponentInChildren<Animator>(); // 애니매이터 컴포넌트
         HP = MaxHp;
     }
@@ -54,12 +62,6 @@ public class PlayerCtrl : MonoBehaviour
             animator.SetBool("Player_Walk", false);
         }
         GunAtClosestMonster();
-        GunTimer += Time.deltaTime;
-        if (GunTimer >= GunRate)
-        {
-            GunTimer = 0f;
-            fire();
-        }
     }
 
     void GunAtClosestMonster()
@@ -80,33 +82,48 @@ public class PlayerCtrl : MonoBehaviour
             }
             return closestMonster;
         }
-        GameObject closestEnemy = FindClosestMonster();
-        if (closestEnemy == null)
-            return;
-        Vector2 newPos = closestEnemy.transform.position - transform.position;
-        float rotZ = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
-        GUN.transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
-        if (closestEnemy.transform.position.x - transform.position.x>0)
+        GameObject closestEnemy = FindClosestMonster();
+        if (closestEnemy != null)
         {
-            GUN.GetComponent<SpriteRenderer>().flipY = false;
+            Vector2 newPos = closestEnemy.transform.position - transform.position;
+            float rotZ = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+            GUN.transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+            if (closestEnemy.transform.position.x - transform.position.x > 0)
+            {
+                GUN.GetComponent<SpriteRenderer>().flipY = false;
+            }
+            else
+            {
+                GUN.GetComponent<SpriteRenderer>().flipY = true;
+            }
+            GunTimer += Time.deltaTime;
+            if (GunTimer >= GunRate)
+            {
+                GunTimer = 0f;
+                fire();
+            }
         }
         else
         {
-            GUN.GetComponent<SpriteRenderer>().flipY = true;
+
         }
     }
+
     void fire()
     {
-        GameObject Bullet = Instantiate(BulletPrefab, GUN.transform.position, GUN.transform.rotation);
-        Bullet.GetComponent<BulletCtrl>().Attacker = gameObject;
+        Instantiate(BulletPrefab, GUN.transform.position, GUN.transform.rotation).GetComponent<BulletCtrl>().Attacker = gameObject;
     }
     public void GetDamage(float Damage)
     {
         HP -= Damage;
         if (HP <= 0)
         {
-            Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(DieSoundClip, transform.position);
+            Destroy(gameObject, 1.0f); //삭제처리 아니고 나중에 부활처리로 할겁니다요
+            return;
         }
+        Destroy(Instantiate(HitEffect, transform.position, Quaternion.identity),0.5f);
     }
 }
