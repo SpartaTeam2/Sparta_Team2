4d85be2265c58e3500a246ff9b8bf8222ef95d6c
gggji1211 <c061211@naver.com>
2025-02-25 14:14:59 +0900

    장비 구분

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -7,88 +7,97 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject itemPrefab;
     public Transform inventoryGrid;
-    public Transform equipGrid;
 
-    private List<GameObject> inventoryItems = new List<GameObject>();
-    private List<GameObject> equippedItems = new List<GameObject>();
+    //  무기, 방어구, 장신구 슬롯 구분
+    public Transform weaponSlot;        // 무기 슬롯
+    public Transform armorSlot;         // 방어구 슬롯
+    public Transform accessorySlot1;    // 장신구 슬롯 1
+    public Transform accessorySlot2;    // 장신구 슬롯 2
 
-    public TextMeshProUGUI playerStatsText; // 플레이어 스탯 UI
+
+    private GameObject equippedWeapon;
+    private GameObject equippedArmor;
+    private GameObject equippedAccessory;
+
+    private Dictionary<GameObject, Item> itemData = new Dictionary<GameObject, Item>();
+    public TextMeshProUGUI playerStatsText;
+
     private int playerAttack = 0;
     private int playerHp = 0;
 
-    private Dictionary<GameObject, Item> itemData = new Dictionary<GameObject, Item>(); // 아이템 데이터 저장
-
     void Start()
     {
-
-             AddItem(new Item("단검", 10, 2, Resources.Load<Sprite>("Icons/weapon_anime_sword")));
-            AddItem(new Item("심장", 7, 1, Resources.Load<Sprite>("Icons/ui_heart_full")));
-            AddItem(new Item("?", 12, 0, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2")));
-            AddItem(new Item("방패", 2, 10, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2")));
-
+        AddItem(new Item("단검", 10, 2, Resources.Load<Sprite>("Icons/weapon_anime_sword"), Item.ItemType.Weapon));
+        AddItem(new Item("심장", 7, 1, Resources.Load<Sprite>("Icons/ui_heart_full"), Item.ItemType.Armor));
+        AddItem(new Item("?", 12, 0, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
+        AddItem(new Item("방패", 2, 10, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
     }
 
     public void AddItem(Item item)
     {
         GameObject newItem = Instantiate(itemPrefab, inventoryGrid);
-
-        Text itemText = newItem.GetComponentInChildren<Text>();
-        if (itemText != null)
-        {
-            itemText.text = item.itemName;
-        }
-
-        Image itemImage = newItem.GetComponentInChildren<Image>(); 
-        if (itemImage != null)
-        {
-            itemImage.sprite = item.itemIcon; 
-        }
-
+        newItem.GetComponentInChildren<Text>().text = item.itemName;
+        newItem.GetComponentInChildren<Image>().sprite = item.itemIcon;
         newItem.GetComponent<Button>().onClick.AddListener(() => EquipItem(newItem));
-        inventoryItems.Add(newItem);
+
         itemData[newItem] = item;
     }
 
-
     public void EquipItem(GameObject item)
     {
-        if (equippedItems.Count >= 4)
-            return;
+        if (!itemData.ContainsKey(item)) return;
 
-        inventoryItems.Remove(item);
-        item.transform.SetParent(equipGrid);
+        Item itemInfo = itemData[item];
+
+        // 각 슬롯별로 장비할 수 있는 아이템 제한
+        switch (itemInfo.itemType)
+        {
+            case Item.ItemType.Weapon:
+                if (equippedWeapon != null) UnequipItem(equippedWeapon);
+                equippedWeapon = item;
+                item.transform.SetParent(weaponSlot);
+                break;
+
+            case Item.ItemType.Armor:
+                if (equippedArmor != null) UnequipItem(equippedArmor);
+                equippedArmor = item;
+                item.transform.SetParent(armorSlot);
+                break;
+        
+               
+        }
+        
+
         item.transform.SetAsLastSibling();
-        equippedItems.Add(item);
-
         item.GetComponent<Button>().onClick.RemoveAllListeners();
         item.GetComponent<Button>().onClick.AddListener(() => UnequipItem(item));
 
-        // 스탯 증가
-        if (itemData.ContainsKey(item))
-        {
-            playerAttack += itemData[item].attack;
-            playerHp += itemData[item].Hp;
-            UpdatePlayerStats();
-        }
+        playerAttack += itemInfo.attack;
+        playerHp += itemInfo.Hp;
+        UpdatePlayerStats();
     }
 
     public void UnequipItem(GameObject item)
     {
-        equippedItems.Remove(item);
+        if (!itemData.ContainsKey(item)) return;
+
+        Item itemInfo = itemData[item];
+
+        switch (itemInfo.itemType)
+        {
+            case Item.ItemType.Weapon: equippedWeapon = null; break;
+            case Item.ItemType.Armor: equippedArmor = null; break;
+            case Item.ItemType.Accessory: equippedAccessory = null; break;
+        }
+
         item.transform.SetParent(inventoryGrid);
         item.transform.SetAsLastSibling();
-        inventoryItems.Add(item);
-
         item.GetComponent<Button>().onClick.RemoveAllListeners();
         item.GetComponent<Button>().onClick.AddListener(() => EquipItem(item));
 
-        // ⭐ 스탯 감소
-        if (itemData.ContainsKey(item))
-        {
-            playerAttack -= itemData[item].attack;
-            playerHp -= itemData[item].Hp;
-            UpdatePlayerStats();
-        }
+        playerAttack -= itemInfo.attack;
+        playerHp -= itemInfo.Hp;
+        UpdatePlayerStats();
     }
 
     void UpdatePlayerStats()
