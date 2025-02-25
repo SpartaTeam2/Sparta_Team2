6e17dd6e74ffc796a7ee56dd82104a6f17193d34
gggji1211 <c061211@naver.com>
2025-02-25 20:17:30 +0900

    슬롯구분완성

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -31,6 +31,10 @@ public class InventoryManager : MonoBehaviour
         AddItem(new Item("심장", 7, 1, Resources.Load<Sprite>("Icons/ui_heart_full"), Item.ItemType.Armor));
         AddItem(new Item("?", 12, 0, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
         AddItem(new Item("방패", 2, 10, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
+        AddItem(new Item("단검", 10, 2, Resources.Load<Sprite>("Icons/weapon_anime_sword"), Item.ItemType.Weapon));
+        AddItem(new Item("심장", 7, 1, Resources.Load<Sprite>("Icons/ui_heart_full"), Item.ItemType.Armor));
+        AddItem(new Item("?", 12, 0, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
+        AddItem(new Item("방패", 2, 10, Resources.Load<Sprite>("Icons/wogol_idle_anim_f2"), Item.ItemType.Accessory));
     }
 
     public void AddItem(Item item)
@@ -49,56 +53,71 @@ public class InventoryManager : MonoBehaviour
 
         Item itemInfo = itemData[item];
 
-        // 각 슬롯별로 장비할 수 있는 아이템 제한
-        switch (itemInfo.itemType)
+        //무기 착용 (한 개만 가능)
+        if (itemInfo.itemType == Item.ItemType.Weapon)
         {
-            case Item.ItemType.Weapon:
-                if (equippedWeapon != null) UnequipItem(equippedWeapon);
-                equippedWeapon = item;
-                item.transform.SetParent(weaponSlot);
-                break;
-
-            case Item.ItemType.Armor:
-                if (equippedArmor != null) UnequipItem(equippedArmor);
-                equippedArmor = item;
-                item.transform.SetParent(armorSlot);
-                break;
-        
-               
+            if (weaponSlot.childCount > 0)
+            {
+                UnequipItem(weaponSlot.GetChild(0).gameObject); // 기존 무기 해제
+            }
+            item.transform.SetParent(weaponSlot);
         }
-        
+        // 방어구 착용 (한 개만 가능)
+        else if (itemInfo.itemType == Item.ItemType.Armor)
+        {
+            if (armorSlot.childCount > 0)
+            {
+                UnequipItem(armorSlot.GetChild(0).gameObject); // 기존 방어구 해제
+            }
+            item.transform.SetParent(armorSlot);
+        }
+        // 장신구 착용 (최대 2개 가능)
+        else if (itemInfo.itemType == Item.ItemType.Accessory)
+        {
+            if (accessorySlot1.childCount == 0)
+            {
+                item.transform.SetParent(accessorySlot1);
+
+            }
+            else if (accessorySlot2.childCount == 0)
+            {
+                item.transform.SetParent(accessorySlot2);
+            }
+            else
+            {
+                return; // 장신구 2개가 모두 차 있으면 착용 불가
+            }
+        }
+        else
+        {
+            return; // 장비가 아니면 착용 불가
+        }
 
         item.transform.SetAsLastSibling();
         item.GetComponent<Button>().onClick.RemoveAllListeners();
         item.GetComponent<Button>().onClick.AddListener(() => UnequipItem(item));
 
+        // 스탯 반영
         playerAttack += itemInfo.attack;
         playerHp += itemInfo.Hp;
         UpdatePlayerStats();
     }
-
     public void UnequipItem(GameObject item)
     {
-        if (!itemData.ContainsKey(item)) return;
-
-        Item itemInfo = itemData[item];
-
-        switch (itemInfo.itemType)
-        {
-            case Item.ItemType.Weapon: equippedWeapon = null; break;
-            case Item.ItemType.Armor: equippedArmor = null; break;
-            case Item.ItemType.Accessory: equippedAccessory = null; break;
-        }
-
         item.transform.SetParent(inventoryGrid);
         item.transform.SetAsLastSibling();
+
         item.GetComponent<Button>().onClick.RemoveAllListeners();
         item.GetComponent<Button>().onClick.AddListener(() => EquipItem(item));
 
-        playerAttack -= itemInfo.attack;
-        playerHp -= itemInfo.Hp;
-        UpdatePlayerStats();
+        if (itemData.ContainsKey(item))
+        {
+            playerAttack -= itemData[item].attack;
+            playerHp -= itemData[item].Hp;
+            UpdatePlayerStats();
+        }
     }
+
 
     void UpdatePlayerStats()
     {
