13afad9c3c7815dd1a0e21f1a233d2a762dd6b0f
min4937 <pmy1504@naver.com>
2025-02-26 17:00:01 +0900

    Fix: 레벨 정상화

--- Assets/UI/Level_Bar.cs
+++ Assets/UI/Level_Bar.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class Level_Bar : MonoBehaviour
@@ -15,58 +12,34 @@ public class Level_Bar : MonoBehaviour
     public float experiencePerSecond = 0f; // 초당 증가하는 경험치 양
 
     void Start()
-    {        
+    {
         experienceSlider.minValue = 0f;
         experienceSlider.maxValue = player.MaxExp;
         experienceSlider.value = player.Exp;
-       
+
         UpdateLevelText();
     }
 
     void Update()
     {
-        
+
         AddExperience(player.Exp);
+        UpdateLevelText();
     }
 
-    
+
     public void AddExperience(float amount)
     {
-        player.Exp += amount;
 
-        
-        if (player.Exp >= player.MaxExp)
-        {
-            LevelUp();
-        }
-        else
-        {
-            
-            experienceSlider.value = player.Exp;
-        }
-    }
 
-    
-    void LevelUp()
-    {
-        //player.level++;
-        //player.Exp -= player.MaxExp;
-        //player.MaxExp *= 1.2f; 
-
-        
-        experienceSlider.maxValue = player.MaxExp;
         experienceSlider.value = player.Exp;
 
-        if (experienceSlider != null)
-        {
-            experienceSlider.maxValue = player.MaxExp; // 슬라이더 최대값 업데이트
-        }
-        UpdateLevelText();
-
-        Debug.Log("레벨 업! 현재 레벨: " + player.level);
     }
 
+
     
+
+
     void UpdateLevelText()
     {
         if (levelText != null)
@@ -75,5 +48,5 @@ public class Level_Bar : MonoBehaviour
         }
     }
 }
-    
+
 
